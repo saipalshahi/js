aa4958cadd6ac34163c4c8670d0df001da4ded7c
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-21 10:13:08 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "saipalmessi@gmail.com"
+var accountPassword = "fdgsdh3784"
+accountCity = "Kathmandu"
+let accountState;
+
+
+// accountId = 2  //not allowed
+{
+
+}
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "fgyeur3784"
+accountCity = "bhaktapur"
+
+console.log(accountId);
+
+// don't use var because of issue in block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treatt all js code as newer version 
+
+// alert( 3 + 3 )  // we are using nodejs , not browser
+
+let name = "Saipal Shahi"
+let age = 19
+let isLogIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol =>unique
+
+
+// object
+
+
+console.log(typeof undefined);
+console.log(typeof null);
