0c41edd0babc3bf8225f2b1e88ee449e33a1970b
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-21 10:38:22 +0000

    converSionOfDataType

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score = "saipal"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33" => NaN
+// true => 1; false => 0
+
+let isLogIn = "Saipal"
+
+let booleanIsLogIn = Boolean(isLogIn)
+console.log(booleanIsLogIn)
+
+// 1 => true; 0 => false
+// " " => false
+// " value " => true
+
+
+let someNumber = 200
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
