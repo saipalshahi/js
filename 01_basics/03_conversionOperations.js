cfdef63e81bc6ada5800b4ddc52b875e754fb56e
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-22 09:20:10 +0000

    conversionOperators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
-// let score = "saipal"
+let score = "saipal"
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -16,7 +16,7 @@
 let isLogIn = "Saipal"
 
 let booleanIsLogIn = Boolean(isLogIn)
-console.log(booleanIsLogIn)
+// console.log(booleanIsLogIn)
 
 // 1 => true; 0 => false
 // " " => false
@@ -26,5 +26,40 @@ console.log(booleanIsLogIn)
 let someNumber = 200
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// =============== Operations ===================
+
+let value = 30
+
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3)
+// console.log(2%3)
+
+
+let str1 = "Saipal"
+let str2 = " Shahi"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 1 + "2")
+// console.log(1 + "1" + 1)
+// console.log("1" + 1 + 1)
+
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
