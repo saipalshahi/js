e462f7a142eb785a480b7ebf791ffcc523444952
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-28 02:46:25 +0000

    nums_and_maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,9 +1,40 @@
+//++++++++++++++++  numbers  +++++++++++++++++++
+
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString())
-console.log(balance.toString().length)
-console.log(balance.toFixed(2));
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.69
+// console.log(otherNumber.toPrecision(3))
+
+
+const hundreds = 1000000000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++  maths +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6));
+// console.log(Math.floor(6.8));
+
+// console.log(Math.random());   //random value
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+
+// dice game
+
+const min = 1; 
+const max = 6;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
