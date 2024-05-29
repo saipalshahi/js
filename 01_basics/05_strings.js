fa1ab8f1f883e45a466428cf899ce294206d0ef4
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-29 12:50:17 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "saipal"
+const repoCount = 30
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String("Saipal-sp")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('p'));
+
+
+const newString  = gameName.substring(0,3)
+// console.log(newString);
+
+
+const anotherString = gameName.slice(-4, 9)
+// console.log(anotherString);
+
+const newStringOne = "   saipal   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://saipalshahi.com/saipal%20shahi"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('saipal'));
+
+
+const myName = "Saipal Shahi"
+// console.log(myName.split(" "));
+// console.log(myName.bold());
