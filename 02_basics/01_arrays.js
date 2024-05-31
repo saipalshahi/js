0705d925ae14eb11ef686cf03df619737794b4d3
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-05-31 18:22:24 +0000

    array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+ // array
+
+ const myArray = [0, 1, 2, 3, 4, 5]
+//  console.log(myArray);
+
+ const myHeros = ["IronMan","Thor","Hulk"]
+//  console.log(myHeros);
+
+ const myArray2 = new Array(1, 2, 3, 4, 5)
+//  console.log(myArray2[0]);
+
+ // Array Methods 
+
+
+//  myArray.push(10)    //add from last
+// myArray.pop()       // remove from last
+
+
+
+// myArray.unshift(1000)    //add from first
+
+// myArray.shift()            // remove from first
+
+// console.log((myArray.includes(20)));
+// console.log(myArray.indexOf(0));
+
+
+const newArray = myArray.join() // jonn helps to bind the array and convert into string
+// console.log(newArray);
+// console.log(typeof newArray);
+
+
+
+// console.log((myArray));
+
+
+
+
+// slice    ,   splice 
+
+// console.log("A", myArray);
+
+const myNewArray1 = myArray.slice(1,3)
+// console.log("B", myArray);
+// console.log(myNewArray1);
+
+const myNewArray2 = myArray.splice(1,3)
+// console.log("C", myArray);
+// console.log(myNewArray2);
