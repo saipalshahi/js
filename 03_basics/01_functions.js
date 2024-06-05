f19cc8fa762d178ecd732fc627114f509cdab7ca
Saipal Shahi Thakuri <115605590+saipalshahi@users.noreply.github.com>
2024-06-05 18:32:19 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -16,12 +16,55 @@ console.log("L");
 // addTwoNumbers (3, "s")
 
 function addTwoNumbers(number1, number2){            
-    let results = number1 + number2
-    console.log("Saipal Shahi");      //  before return it will give output
-    return results
-    console.log("saipal");      // after return it don't give output
+    // let results = number1 + number2
+    // console.log("Saipal Shahi");      //  before return it will give output
+    // return results
+    // console.log("saipal");      // after return it don't give output
+
+    return number1 + number2   // easy way to return
 }
 
 const results = addTwoNumbers(3, 5)
 
-console.log("value : ", results);
+// console.log("value : ", results);
+
+
+function loginUserMessage(username = "shyam"){         //default value
+    if(username === undefined){
+        console.log("please enter the username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("saipal"))
+// console.log(loginUserMessage("saipal"))   //passed value
+
+function calculateCardPrice (...num1){         // ... is rest operator and spread operator
+    return num1
+}
+
+// console.log(calculateCardPrice(200, 400, 500, 800));
+
+
+function calculateCardPrice (val1, val2 ,...num1){        
+    return num1
+}
+
+// console.log(calculateCardPrice(200, 400, 500, 800));
+
+const user = {
+    username: "saipal",
+     price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}.`);
+}
+// handleObject(user)
+
+handleObject({
+    username: "himal",
+    price: 299
+})
+
+
